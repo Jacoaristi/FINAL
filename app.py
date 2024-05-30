768871ae73ff09636a9106d18c9121e1738df441
Jacoaristi <160860995+Jacoaristi@users.noreply.github.com>
2024-05-30 18:43:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+import streamlit as st
+import requests
+import pandas as pd
+import time
+
+st.title('Monitor de Temperatura y Humedad')
+
+temperature = st.empty()
+humidity = st.empty()
+chart_data = st.empty()
+
+data_points = []
+
+def get_data():
+    response = requests.get('https://wokwi.com/projects/399165862335351809')  
+    return response.json()
+
+while True:
+    data = get_data()
+    temp = data['temperature']
+    hum = data['humidity']
+
+    temperature.metric("Temperatura", f"{temp} °C")
+    humidity.metric("Humedad", f"{hum} %")
+
+    data_points.append([time.time(), temp, hum])
+    df = pd.DataFrame(data_points, columns=['Time', 'Temperature', 'Humidity'])
+
+    chart_data.line_chart(df.set_index('Time'))
+
+    time.sleep(2)
+
 
